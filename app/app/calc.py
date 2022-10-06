12e92f7175545d4c8e02b25c07d89185fb218b4a
carljduff <carljduff@gmail.com>
2022-10-06 19:20:37 -0400

    configured docker compose to wait for database

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,4 +6,7 @@ Calculator functions
 def add(x, y):
     """Add Function"""
     return x + y
-    
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,6 +3,7 @@ Sample tests
 """
 
 from django.test import SimpleTestCase
+from rest_framework.test import APIClient
 
 from app import calc
 
@@ -15,3 +16,8 @@ class CalcTests(SimpleTestCase):
         """Test that two numbers are added together"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test that two numbers are subtracted together"""
+        res = calc.subtract(5, 6)
+        self.assertEqual(res, -1)
